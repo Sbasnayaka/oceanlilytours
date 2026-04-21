9ceccc6e6bf23a22299065a25c10e93a167f0b10
Sanduni Basnayake <sandunikbasnayaka@gmail.com>
2026-04-21 14:11:49 +0530

    fix th API key issue

--- backend_laravel/resources/views/admin/packages/create.blade.php
+++ backend_laravel/resources/views/admin/packages/create.blade.php
@@ -178,8 +178,8 @@
 
 @stack('scripts')
 @push('scripts')
-<!-- TinyMCE Script -->
-<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+<!-- TinyMCE Script (Self-hosted Open Source version from CDNJS to bypass API key) -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/6.8.2/tinymce.min.js"></script>
 <script>
     tinymce.init({
         selector: '.rich-editor',
@@ -188,7 +188,7 @@
         plugins: [
             'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
             'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+            'insertdatetime', 'media', 'table', 'help', 'wordcount'
         ],
         toolbar: 'undo redo | blocks | ' +
         'bold italic underline | alignleft aligncenter ' +

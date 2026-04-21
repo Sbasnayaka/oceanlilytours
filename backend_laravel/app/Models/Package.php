497083cf9c9ea6ef5829efb4a2cc8bf43a1fe07f
Sanduni Basnayake <sandunikbasnayaka@gmail.com>
2026-04-21 15:55:47 +0530

    fix the package sectikon issues

--- backend_laravel/app/Models/Package.php
+++ backend_laravel/app/Models/Package.php
@@ -34,4 +34,16 @@ class Package extends Model
     {
         return $this->hasMany(PackageItinerary::class, 'package_id')->orderBy('day_number', 'asc');
     }
+
+    /**
+     * Automatically convert relative upload paths to absolute URLs for the API
+     */
+    public function getImageUrlAttribute($value)
+    {
+        if ($value && !filter_var($value, FILTER_VALIDATE_URL)) {
+            // Converts /uploads/packages/img.jpg to https://jerzy.lk/oceanlilly/backend_laravel/public/uploads/packages/img.jpg
+            return url($value);
+        }
+        return $value;
+    }
 }
--- backend_laravel/app/Models/PackageItinerary.php
+++ backend_laravel/app/Models/PackageItinerary.php
@@ -18,4 +18,15 @@ class PackageItinerary extends Model
     {
         return $this->belongsTo(Package::class, 'package_id');
     }
+
+    /**
+     * Automatically convert relative upload paths to absolute URLs for the API
+     */
+    public function getImageUrlAttribute($value)
+    {
+        if ($value && !filter_var($value, FILTER_VALIDATE_URL)) {
+            return url($value);
+        }
+        return $value;
+    }
 }
